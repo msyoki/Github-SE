2ff20d6856f2665ecf687b83d6e09c38bb193193
Msyoki <msyokimutua@gmail.com>
2020-09-02 00:56:17 +0300

    update declarations for new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { RepositoriesComponent } from './repositories/repositories/repositories.component';
+import { SearchFormComponent } from './search-form/search-form/search-form.component';
+import { SearchComponent } from './user/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RepositoriesComponent,
+    SearchFormComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
